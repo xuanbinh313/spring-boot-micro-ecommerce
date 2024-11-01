0c2a50acdfa12b59c71f4b88b5ae8a6247d72d88
BinhTX <binh.xuantran@digicommercegroup.com>
2024-11-01 09:29:06 +0700

    first commit

---
+++ product-service/src/main/java/com/binhcodev/spring_boot_micro_product_service/SpringBootMicroProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.binhcodev.spring_boot_micro_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroProductServiceApplication.class, args);
+	}
+
+}
---
+++ product-service/src/test/java/com/binhcodev/spring_boot_micro_product_service/SpringBootMicroProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binhcodev.spring_boot_micro_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
